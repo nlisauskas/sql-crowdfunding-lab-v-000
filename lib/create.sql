1031ee84a37b6b1acaa9a3b97baa91c91277212d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-24 21:30:54 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE projects title TEXT, category TEXT, funding_goal INTEGER, start_date DATE, end_date DATE;
+CREATE TABLE user name TEXT, age INTEGER;
+CREATE TABLE pledge amount INTEGER, user_id INTEGER, project_id INTEGER;
