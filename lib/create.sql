b070dccaf2043952f34f115328ea45f8dbcb16ac
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-24 21:32:26 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,3 @@
-CREATE TABLE projects id PRIMARY KEY INTEGER, title TEXT, category TEXT, funding_goal INTEGER, start_date DATE, end_date DATE;
+CREATE TABLE projects id PRIMARY KEY INTEGER, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT;
 CREATE TABLE user id PRIMARY KEY INTEGER, name TEXT, age INTEGER;
 CREATE TABLE pledge amount INTEGER, user_id INTEGER, project_id INTEGER;
